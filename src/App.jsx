2e8a638ca36daa5e7717072bc9994a63da4ad231
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-12-11 14:06:00 +0100

    Adds files to declare module commerce.js. Trying to solve error with the module. WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
 import { Products, Navbar } from "./components";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetProducts();
+  }, []);
+  console.log(products);
+
   return (
     <div>
       <Navbar />
